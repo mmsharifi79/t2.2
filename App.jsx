087d74c3465687da24a2148bff99145a464c1231
mmsharifi79 <129947258+mmsharifi79@users.noreply.github.com>
2023-06-21 09:28:47 +0330

    Update App.jsx

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CharacterCounter from './CharacterCounter';
+
+function App() {
+  return (
+    <div className="App">
+      <CharacterCounter />
+    </div>
+  );
+}
+
+export default App;
